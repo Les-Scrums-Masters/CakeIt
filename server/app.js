c8b34277dbebeba3c81259b2fb53b27d73f35bc1
Eren Mustafa Ceylan <eren-mustafa.ceylan@etu.unistra.fr>
2022-04-06 23:02:43 +0200

    Connection between client and server

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-const port = process.env.PORT || 4001;
+const port = 3001;
 //const index = require("./routes/index");
 
 const app = express();
@@ -13,15 +13,16 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: `http://localhost:${port}`,
+    origin: `http://localhost:3000`,
+    method: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`User connected : ${socket.id}`);
 
   socket.on("disconnect", () => {
-    console.log(`User ${socket.id} disconnected`);
+    console.log(`User disconnected : ${socket.id}`);
   });
 });
 
