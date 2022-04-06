68868ae674e967a2c6b4a81739f64364050fba2b
Eren Mustafa Ceylan <eren-mustafa.ceylan@etu.unistra.fr>
2022-04-06 22:50:41 +0200

    Added io server connection

--- server/app.js
+++ server/app.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const http = require("http");
-const socketIo = require("socket.io");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const port = process.env.PORT || 4001;
-const index = require("./routes/index");
+//const index = require("./routes/index");
 
 const app = express();
-app.use(index);
+app.use(cors());
 
 const server = http.createServer(app);
 
-const io = socketIo(server); // < Interesting!
+const io = new Server(server, {
+  cors: {
+    origin: `http://localhost:${port}`,
+  },
+});
 
-const getApiAndEmit = "TODO";
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("disconnect", () => {
+    console.log(`User ${socket.id} disconnected`);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Listening on http://localhost:${port}/`);
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,9 @@ router.get("/", (req, res) => {
   res.send({ response: "I am alive" }).status(200);
 });
 
+router.get("/games", (req, res) => {
+  //res.send({ response: "coucou" }).status(200);
+  res.sendFile(path.join(__dirname, "../../pec22/src/app.js"));
+});
+
 module.exports = router;
