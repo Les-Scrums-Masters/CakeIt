54deafe26ce869dd9dd55aa46d38e0ccd27a9744
Eren Mustafa Ceylan <eren-mustafa.ceylan@etu.unistra.fr>
2022-04-06 22:21:44 +0200

    Init of the server side

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server); // < Interesting!
+
+const getApiAndEmit = "TODO";
+
+server.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}/`);
+});
