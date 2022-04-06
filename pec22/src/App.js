981f0add69bcd7f6eb9c12d60521e9e203cb79e7
Franck G <fgutmann@etu.unistra.fr>
2022-04-06 11:42:10 +0200

    Initialisation du projet React avec Tailwind et DaisyUI

---
+++ pec22/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>hey</h1>
+        <button class="btn">Button</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
