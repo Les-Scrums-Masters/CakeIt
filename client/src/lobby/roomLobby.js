042ecfb54a71d52358db60e245fb769d0951db0e
Eren Mustafa Ceylan <eren-mustafa.ceylan@etu.unistra.fr>
2022-04-07 16:31:59 +0200

    roomLobby join and quit

--- client/src/lobby/roomLobby.js
+++ client/src/lobby/roomLobby.js
@@ -6,6 +6,7 @@ export default function RoomLobby(props) {
 
   useEffect(() => {
     props.socket.on("refresh_players", (list) => {
+      console.log(list);
       setPlayers(list);
     });
 
@@ -27,20 +28,25 @@ export default function RoomLobby(props) {
     props.setDisplay("HomePage");
   }
 
+  let btnStart = null;
+  if (props.room.players[0].id === props.playerId) {
+    btnStart = (
+      <button className="btn btn-success" onClick={startGame}>
+        Commencer la partie
+      </button>
+    );
+  }
+
   return (
     <div className="bg-grey justify container mx-auto flex h-full w-full flex-col items-center justify-center gap-20 align-middle">
       <div className="grid">
         <p className="text-center text-neutral">Salon</p>
         <h1 className="text-6xl font-bold text-error">{props.room.roomCode}</h1>
       </div>
-
+      {btnStart}
       <LobbyPlayerList players={players} />
 
       <div className="flex flex-col gap-3">
-        <button className="btn btn-success" onClick={startGame}>
-          Commencer la partie
-        </button>
-
         <button className="btn btn-link" onClick={back}>
           Retour
         </button>
