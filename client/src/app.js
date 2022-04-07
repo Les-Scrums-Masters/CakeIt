900cbf532670a97a71eb5cac244d62c2d0f06a36
Clément Nogueira <clement.nogueira@etu.unistra.fr>
2022-04-07 14:45:26 +0200

    changing axis and add legend on charts

--- client/src/app.js
+++ client/src/app.js
@@ -2,11 +2,12 @@ import io from "socket.io-client";
 import React from "react";
 import Homepage from "./home/homepage";
 import RoomLobby from "./lobby/roomLobby";
+import Chart from "./charts/chart.js";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return <Homepage socket={socket} />;
+  return <Chart />;
 }
 
 export default App;
