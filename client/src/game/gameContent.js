e9f3e09d87df5ea3a5e51f918c5a220dc67c206d
GUTMANN FRANCK <fgutmann@etu.unistra.fr>
2022-04-07 22:04:06 +0200

    Added display of ingredient prices (without design)

--- client/src/game/gameContent.js
+++ client/src/game/gameContent.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Chart from "../charts/chart";
 import Slider from "../components/slider";
+import IngredientItem from "./ingredientItem";
 import ValueDisplay from "./valueDisplay";
 
 /* PROPS : 
@@ -9,10 +10,12 @@ import ValueDisplay from "./valueDisplay";
             roomCode={props.room.roomCode}
 */
 export default function GameContent(props) {
-  const [isReport, setIsReport] = useState(false);
+  const [isReport, setIsReport] = useState(true);
 
   const [production, setProduction] = useState(5);
   const [price, setPrice] = useState(5);
+
+  const [ingredients, setIngredients] = useState({});
 
   const ready = () => {
     let data = {
@@ -29,6 +32,9 @@ export default function GameContent(props) {
     props.socket.on("end_day", () => {
       setIsReport(true);
     });
+    props.socket.on("update_ingredients", (data) => {
+      setIngredients(data);
+    });
   });
 
   let volumes = props.player.volume.values;
@@ -40,24 +46,29 @@ export default function GameContent(props) {
   if (prices.length > 0) lastPrice = prices[prices.length - 1];
   if (volumes.length > 0) lastVolume = volumes[volumes.length - 1];
 
-  return (
-    <div className="flex h-full w-full flex-col gap-5 rounded-xl border-2 border-success bg-white p-8">
-      <h1 className="text-4xl font-bold text-success">
-        {isReport ? "Bilan" : "Vente en cours ..."}
-      </h1>
-
-      <div className="flex flex-row text-success">
-        <ValueDisplay value={lastVolume} legend="gateaux en rayon" />
-        <ValueDisplay
-          value={lastPrice}
-          legend="prix de vente du gateau"
-          suffix=" €"
-        />
-      </div>
-
+  let content;
+  if (isReport) {
+    content = (
       <div className="divide grid flex-1 grid-cols-3 items-stretch gap-4 divide-x">
         <div className="col-span-2 row-span-3 h-full bg-info">Charts</div>
         <div className="row-span-2 flex flex-col pl-3 align-middle">
+          <h3 className="text-2xl font-bold text-success">
+            Coût des ingrédients
+          </h3>
+
+          {Object.values(ingredients).map((item) => {
+            let price = 0;
+            let evolution = 0;
+
+            return (
+              <IngredientItem
+                name={item.name}
+                price={price}
+                evolution={evolution}
+              />
+            );
+          })}
+
           <h3 className="text-2xl font-bold text-success">
             Ajustez votre production
           </h3>
@@ -80,12 +91,33 @@ export default function GameContent(props) {
             onChange={(v) => setProduction(v)}
           />
         </div>
-        <div className="flex justify-end">
+        <div className="flex place-items-end justify-end">
           <button className="btn btn-success" onClick={ready}>
             Valider
           </button>
         </div>
       </div>
+    );
+  } else {
+    content = "Vente ...";
+  }
+
+  return (
+    <div className="flex h-full w-full flex-col gap-5 rounded-xl border-2 border-success bg-white p-8">
+      <h1 className="text-4xl font-bold text-success">
+        {isReport ? "Bilan" : "Vente en cours ..."}
+      </h1>
+
+      <div className="flex flex-row text-success">
+        <ValueDisplay value={lastVolume} legend="gateaux en rayon" />
+        <ValueDisplay
+          value={lastPrice}
+          legend="prix de vente du gateau"
+          suffix=" €"
+        />
+      </div>
+
+      {content}
     </div>
   );
 }
