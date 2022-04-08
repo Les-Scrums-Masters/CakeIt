462a795bac4659b3f8d638ab844863fe5a14eeca
Eren Mustafa Ceylan <eren-mustafa.ceylan@etu.unistra.fr>
2022-04-08 10:02:15 +0200

    Resize chart on endpage

--- client/src/end/endPage.js
+++ client/src/end/endPage.js
@@ -15,7 +15,9 @@ export default function EndPage(props) {
       </div>
       {console.log(props.room.players)}
       <EndPlayerList players={props.room.players} />
-      <EndChart players={props.room.players} />
+      <div className="h-[200px]">
+        <EndChart players={props.room.players} />
+      </div>
       <div className="flex flex-col gap-3">
         <button className="btn btn-link" onClick={back}>
           Retour au menu principal
